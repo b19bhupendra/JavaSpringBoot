e2237a6ac6e3db3a4f98dc1b5ef4cb7bfcf1a2ed
bhupendra <bhupendrayadav2732@gmail.com>
2025-01-06 01:11:57 +0530

    implementing get all the courses api method

---
+++ springRest/src/main/java/com/springRestApi/springRest/services/IcourseService.java
@@ -0,0 +1,12 @@
+package com.springRestApi.springRest.services;
+
+import java.util.List;
+
+import com.springRestApi.springRest.entities.Course;
+
+
+public interface IcourseService {
+	
+	public List<Course> getCourses();//this will call its child class body
+ 
+}
